acb847618f13b4c516956d039fcb90c3ae2f3e4e
crypt0-1 <prajwal>
2020-05-27 23:24:15 +0530

    Updates

---
+++ precache-manifest.51c31aabb610a8f024a28bfbc339d580.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8930863e14e20ca5fb5a0ed9167fd42",
+    "url": "/portfolio-v1/index.html"
+  },
+  {
+    "revision": "9cd7618ecce8243c4d0b",
+    "url": "/portfolio-v1/static/css/main.4b6335e5.chunk.css"
+  },
+  {
+    "revision": "857d552782960852eab2",
+    "url": "/portfolio-v1/static/js/2.1268be0c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio-v1/static/js/2.1268be0c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9cd7618ecce8243c4d0b",
+    "url": "/portfolio-v1/static/js/main.9cbc8671.chunk.js"
+  },
+  {
+    "revision": "4f0dd5c2413c496c7bcc",
+    "url": "/portfolio-v1/static/js/runtime-main.49177566.js"
+  },
+  {
+    "revision": "dcd12414a497ae3644c693b0ac652576",
+    "url": "/portfolio-v1/static/media/Restaurant.dcd12414.PNG"
+  },
+  {
+    "revision": "e74f5aaa56a7190e568aad86e5fa93bc",
+    "url": "/portfolio-v1/static/media/computer.e74f5aaa.png"
+  },
+  {
+    "revision": "aeba9664af8c0cfe6146cebb1c30a0fd",
+    "url": "/portfolio-v1/static/media/ham.aeba9664.svg"
+  },
+  {
+    "revision": "8caaf1d6cbf3ef2dc087f8a5b099ac96",
+    "url": "/portfolio-v1/static/media/screenshot.8caaf1d6.png"
+  },
+  {
+    "revision": "761230efda71a9fe2c53f3d8cd2a25af",
+    "url": "/portfolio-v1/static/media/skill.761230ef.png"
+  },
+  {
+    "revision": "311a92243e3edfb91e8371b495f0a39b",
+    "url": "/portfolio-v1/static/media/you.311a9224.png"
+  }
+]);
